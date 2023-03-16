26b3c26167eab67ce7f7ddbabd2beb11e04225ba
damlaYasarr <damlayasar40@gmail.com>
2023-03-17 01:24:06 +0300

    post bug

--- frontend/src/app/services/entryservices.ts
+++ frontend/src/app/services/entryservices.ts
@@ -1,11 +1,13 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-
+import { Observable } from "rxjs";
+import { catchError, map } from 'rxjs/operators';
 @Injectable({
     providedIn:'root'
 })
 
 export class EntryServices {
+    private baseUrl = 'https://localhost:7095/api/TagEntry';
     constructor(private httpclient:HttpClient){}
     public tagidforentities: number;
     getEntry(){
@@ -22,9 +24,21 @@ export class EntryServices {
     getTagsAllEntriesByTagId(id:number){
         return this.httpclient.get('https://localhost:7095/api/TagEntry/entries'+id);
     }
+
     addEntry(userid:number, tag_id:number, def:string){
-        return this.httpclient.post('https://localhost:7095/api/TagEntry/entryekle', {userid, tag_id, def});
-    }
+        const body = { userid, tag_id, def };
+        console.log(body);
+        return this.httpclient.put<any>(`${this.baseUrl}/entryekle`, body).pipe(
+          map(user => {
+            console.log(user);
+          }),
+          catchError(error => {
+            console.error(error);
+            throw error; // re-throw the error to propagate it to the subscriber
+          })
+        );
+      }
+    
     searchtagandname(nn:string){
         return this.httpclient.get(`https://localhost:7095/api/TagEntry/search?nn=`+nn);
     }
